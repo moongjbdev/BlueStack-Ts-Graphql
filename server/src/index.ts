1fe7fd5d7d68c10c7b212673aa27c9a378716ec4
moongjbdev <moongjbdev@gmail.com>
2025-02-12 22:07:17 +0700

    setup session with mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,12 +6,15 @@ import { User } from "./entities/User";
 import { Post } from "./entities/Post";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
-import {
-  ApolloServerPluginLandingPageGraphQLPlayground,
-  Context,
-} from "apollo-server-core";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import { HelloResolver } from "./resolvers/hello";
 import { UserResolver } from "./resolvers/user";
+import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import { __prod__, COOKIE_NAME } from "./constants";
+import { Context } from "./types/Context";
+import cors from "cors";
 const main = async () => {
   await createConnection({
     type: "postgres",
@@ -22,8 +25,42 @@ const main = async () => {
     synchronize: true,
     entities: [User, Post],
   });
+
   const app = express();
 
+  //session cookies
+  const mongoURI =
+    process.env.DB_MONGODB_SESSION || "mongodb://localhost:27017/";
+  await mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  // app.use(
+  //   cors({
+  //     origin: "http://localhost:4000", // FE của bạn
+  //     credentials: true, // Quan trọng để gửi cookie
+  //   })
+  // );
+
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: MongoStore.create({ mongoUrl: mongoURI }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+        httpOnly: true, //JS from FE cannot read cookies
+        secure: __prod__, // Cookie only sent over HTTPS
+        sameSite: "lax", // Protection against CSRF attacks,
+      },
+      secret:
+        (process.env.SESSION_SECRET_DEV_PROD as string) || "my-secret-key",
+      resave: false,
+      saveUninitialized: false, // dont save empty values session
+    })
+  );
+
+  // Apollo Server setup
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, UserResolver],
@@ -34,6 +71,7 @@ const main = async () => {
   });
 
   await apolloServer.start();
+
   apolloServer.applyMiddleware({ app, cors: false });
 
   const PORT = process.env.PORT || 4000;
--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,10 +1,12 @@
 import { User } from "../entities/User";
-import { Resolver, Mutation, Arg } from "type-graphql";
+import { Resolver, Mutation, Arg, Ctx } from "type-graphql";
 import argon2 from "argon2";
 import { UserMutationResponse } from "../types/UserMutationResponse";
 import { RegisterInput } from "../types/RegisterInput";
 import { validateregisterInput } from "../utils/validateRegisterInput";
 import { LoginInput } from "../types/LoginInput";
+import { Context } from "../types/Context";
+import { COOKIE_NAME } from "../constants";
 @Resolver()
 export class UserResolver {
   @Mutation((_return) => UserMutationResponse)
@@ -66,7 +68,8 @@ export class UserResolver {
   }
   @Mutation((_return) => UserMutationResponse)
   async login(
-    @Arg("loginInput") loginInput: LoginInput
+    @Arg("loginInput") loginInput: LoginInput,
+    @Ctx() { req, res }: Context
   ): Promise<UserMutationResponse> {
     try {
       const { usernameOrEmail, password } = loginInput;
@@ -105,6 +108,23 @@ export class UserResolver {
           ],
         };
       }
+      console.log("Log bug 1");
+      //session: userId = existingUser.id
+      //create session and return cookie
+      req.session.userId = existingUser.id;
+      console.log("Session after setting userId:", req.session);
+      console.log("Response headers:", res.getHeaders());
+
+      //set-cookie
+      res.cookie(COOKIE_NAME, req.sessionID, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      });
+
+      // return user data
+      existingUser.updatedAt = new Date();
+      await existingUser.save();
+
       return {
         code: 200,
         success: true,
